61a89b7db85ad1c340aa758e438ea21dc5207a23
ballardinimarta <marta.ballardini@gmail.com>
2022-01-13 12:00:49 +0100

    new database file again

--- doktordrej-backend/config/env/production/database.js
+++ doktordrej-backend/config/env/production/database.js
@@ -1,20 +1,17 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-       //add this line
-       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },     
-    },
-    // add this line
-    options: {
-      ssl: env.bool('DATABASE_SSL', false),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
     debug: false,
   },
